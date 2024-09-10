7538143ee973d82ff7969ee73fb68b4152764bce
PietroViglino <157515807+PietroViglino@users.noreply.github.com>
2024-09-10 17:19:28 +0200

    implemented split requests

--- wms2tiff.py
+++ wms2tiff.py
@@ -3,6 +3,9 @@ from xml.etree import ElementTree as ET
 import requests
 import numpy as np
 import os
+import warnings
+
+warnings.filterwarnings("ignore")
 
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
@@ -93,10 +96,22 @@ def get_capabilities(base_url, use_token=False):
     maxx = capabilities.find('.//ns0:BoundingBox', namespace).attrib.get('maxx')
     miny = capabilities.find('.//ns0:BoundingBox', namespace).attrib.get('miny')
     maxy = capabilities.find('.//ns0:BoundingBox', namespace).attrib.get('maxy')
-    cap_dict['bbox']= f'{miny},{minx},{maxy},{maxx}'
+
+    cenx = str((float(minx) + float(maxx))/2)
+    ceny = str((float(miny) + float(maxy))/2)
+
+    cap_dict['bboxes'] = [
+        # Top-left quadrant (min y, min x, center y, center x)
+        f'{miny},{minx},{ceny},{cenx}',  
+        # Top-right quadrant (min y, center x, center y, max x)
+        f'{miny},{cenx},{ceny},{maxx}',  
+        # Bottom-left quadrant (center y, min x, max y, center x)
+        f'{ceny},{minx},{maxy},{cenx}',  
+        # Bottom-right quadrant (center y, center x, max y, max x)
+        f'{ceny},{cenx},{maxy},{maxx}'  
+    ]
 
     cap_dict['title'] = capabilities.find('.//ns0:Title', namespace).text
-
     return cap_dict
 
 
@@ -131,7 +146,6 @@ def set_transparency(input_tiff, output_tiff, transparency):
         'COMPRESS=LZW',  
         'TILED=YES',
         'ALPHA=YES'
-        
     ])
 
     out_dataset.SetGeoTransform(geotransform)
@@ -145,7 +159,27 @@ def set_transparency(input_tiff, output_tiff, transparency):
     out_dataset = None
 
 
+def merge_tiffs(files, output_file):
+    try:
+        print("Merging TIFF files...")
+
+        warp_options = gdal.WarpOptions(
+            format='GTiff',
+            creationOptions=["COMPRESS=LZW", "TILED=YES", "ALPHA=YES"]
+        )
+
+        gdal.Warp(destNameOrDestDS=output_file, srcDSOrSrcDSTab=files, options=warp_options)
+
+        print(f"GeoTIFF files merged successfully into {output_file}.")
+
+        for file in files:
+            if os.path.exists(file):
+                os.remove(file)
+    except Exception as e:
+        print(f"Error during merging: {str(e)}")
+
 def download_wms_layer(wms_url, output_tiff):
+    output_files = []
     try:
         capabilities = get_capabilities(wms_url, use_token=True)
 
@@ -155,47 +189,51 @@ def download_wms_layer(wms_url, output_tiff):
         width = max_width
         height = max_height
 
-        width, height = 5000, 5000 
-        # working up until 50000, 50000. with higher values server goes into timeout
-        # splitting requests into chunks and merging the output 
+        width, height = 50000, 50000
 
-        bbox = capabilities['bbox']
-        
-        if capabilities['time']:
-            time = capabilities['time']
+        for idx, bbox in enumerate(capabilities['bboxes']):
+            
+            if capabilities['time']:
+                time = capabilities['time']
 
-        options = [
-            '-co', 'ALPHA=YES',
-            '-co', 'TILED=YES',
-            '-co', 'COMPRESS=LZW'
-        ]
+            options = [
+                '-co', 'ALPHA=YES',
+                '-co', 'TILED=YES',
+                '-co', 'COMPRESS=LZW'
+            ]
 
-        g_token = get_token()
+            g_token = get_token()
 
-        wms_url_with_size = f'{wms_url}?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&styles=default&LAYERS=0&WIDTH={width}&HEIGHT={height}\
-            &FORMAT=image/png&TRANSPARENT=true&CRS=EPSG:4326&BBOX={bbox}&token={g_token}'
+            wms_url_with_size = f'{wms_url}?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&styles=default&LAYERS=0&WIDTH={width}&HEIGHT={height}\
+                &FORMAT=image/png&TRANSPARENT=true&CRS=EPSG:4326&BBOX={bbox}&token={g_token}'
 
-        wms_dataset = gdal.Open(wms_url_with_size)
+            wms_dataset = gdal.Open(wms_url_with_size)
 
-        if wms_dataset is None:
-            print("GDAL failed to open the WMS dataset.")
-            return
-        
-        # print_dataset_info(wms_dataset)
-        print('gdal.Translate...')
-        gdal.Translate(output_tiff, wms_dataset, format='GTiff', width=width, height=height, options=options)
-        print('Elaborating tiff file...')
-        set_transparency(output_tiff, output_tiff.replace('.tiff', '_transp.tiff'), transparency=0.5)
+            if wms_dataset is None:
+                print("GDAL failed to open the WMS dataset.")
+                return
+            
+            print('gdal.Translate...')
+            i = idx + 1
+            gdal.Translate(output_tiff.replace('.tiff', f'_{i}.tiff'), wms_dataset, format='GTiff', width=width, height=height, options=options)
+            output_files.append(output_tiff.replace('.tiff', f'_{i}.tiff'))
+            print('Elaborating tiff file...')
+            set_transparency(output_tiff.replace('.tiff', f'_{i}.tiff'), output_tiff.replace('.tiff', f'_{i}.tiff').replace('.tiff', '_transp.tiff'), transparency=0.5)
+            output_files.remove(output_tiff.replace('.tiff', f'_{i}.tiff'))
+            if output_tiff.replace('.tiff', f'_{i}.tiff') in os.listdir():
+                os.remove(output_tiff.replace('.tiff', f'_{i}.tiff'))
+            output_files.append(output_tiff.replace('.tiff', f'_{i}.tiff').replace('.tiff', '_transp.tiff'))
 
-        print(f"Downloaded WMS layer and saved as {output_tiff}")
+        merge_tiffs(output_files, output_tiff)
     except Exception as e:
         print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
     
-    base_wms_url = 'https://webgis.abdac.it/server/services/ETT_PERIC_FRANA_DISTR/MapServer/WMSServer'
+    # base_wms_url = 'https://webgis.abdac.it/server/services/ETT_PERIC_FRANA_DISTR/MapServer/WMSServer'
     # base_wms_url = 'https://webgis.abdac.it/server/services/Pericolosit%C3%A0_idraulica_Progetto_PAI_distr___solo_fluviale___bozza_/MapServer/WMSServer'
 
+    base_wms_url = input('Please write the URL to the WMS Layer:\n')
     output_path = input('Please write a name for the output .tiff file:\n')
     if not output_path.endswith('.tiff'):
         output_path += '.tiff'
